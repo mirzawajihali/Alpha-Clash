524d6319656136eaec958b3d8c93ee8b56c27078
mirzawajihali <mirzawajihali00@gmail.com>
2025-01-19 00:58:52 +0600

    almost done

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -7,11 +7,60 @@
 // }
 
 
-function play(){
-    hideByID("home-screen");
-    showByID("play-ground");
-    continueGame();
+
+
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key;
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+
+    const currentAlphabet = currentAlphabetElement.innerText.toLowerCase();
+
+    if(playerPressed === currentAlphabet){
+        console.log("Congratulations");
+
+        // update score 
+
+        const currentScoreElement = document.getElementById("current-score");
+        const currentScore = parseInt(currentScoreElement.innerText);
+
+        const newScore = currentScore+1;
+
+        currentScoreElement.innerText = newScore;
+
+        
+        
+
+
+
+
+        // starting a new round 
+        continueGame();
+        removeBackgroundColorById(currentAlphabet)
+
+    }
+    else{
+        console.log("hahahha you lost");
+
+        const lifeElement = document.getElementById('life');
+
+        const life = parseInt(lifeElement.innerText);
+        const newLife = life-1;
+
+        lifeElement.innerText =newLife;
+
+        if(newLife === 0){
+            gameOver();
+        }
+
+    }
+
+
+
 }
+
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
 
 
 function continueGame(){
@@ -26,4 +75,19 @@ function continueGame(){
 
 
     setBackgroundColorById(alphabet);
+}
+
+ 
+
+function play(){
+    hideByID("home-screen");
+    hideByID("score-section");
+    showByID("play-ground");
+    continueGame();
+}
+
+function gameOver(){
+    hideByID("play-ground");
+    showByID("score-section");
+    
 }
